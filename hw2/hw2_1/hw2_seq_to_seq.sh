5da025802522c7accf4e9134d9da6714d55fb22e
Tarun Upputuri <92453558+TarunUpp@users.noreply.github.com>
2023-03-04 16:11:34 -0500

    Update hw2_seq_to_seq.sh

--- hw2/hw2_1/hw2_seq_to_seq.sh
+++ hw2/hw2_1/hw2_seq_to_seq.sh
@@ -1,2 +1,3 @@
 python sequence.py /home/tupputu/MLDS_hw2_1_data/training_data/feat/ /home/tupputu/MLDS_hw2_1_data/training_label.json
 python train.py /home/tupputu/MLDS_hw2_1_data/training_data/feat/ /home/tupputu/MLDS_hw2_1_data/training_label.json ./tupputu_output.txt
+wget https://www.dropbox.com/s/tsppbquna6bouzo/model4411-4.data-00000-of-00001?dl=0
