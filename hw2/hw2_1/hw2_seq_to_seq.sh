cbc2b9426f691b015737e54123ff4464f39418c6
Tarun Upputuri <92453558+TarunUpp@users.noreply.github.com>
2023-03-04 13:21:55 -0500

    hw2_seq_to_seq.sh

---
+++ hw2/hw2_1/hw2_seq_to_seq.sh
@@ -0,0 +1,2 @@
+python sequence.py /home/tupputu/MLDS_hw2_1_data/training_data/feat/ /home/tupputu/MLDS_hw2_1_data/training_label.json
+python train.py /home/tupputu/MLDS_hw2_1_data/training_data/feat/ /home/tupputu/MLDS_hw2_1_data/training_label.json ./tupputu_output.txt
